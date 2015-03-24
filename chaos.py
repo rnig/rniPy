ef5761a8b9215cd0969a6f3cc99d9c7674572452
Erwin Geeraerts <erwin@rnig.be>
2015-03-24 10:04:58 +0000

    a chaotic python starter

---
+++ chaos.py
@@ -0,0 +1,12 @@
+# @naam: chaos.py
+# @wat: demo chaotisch gedrag .
+
+def main():
+    print("This program illustrates a chaotic function")
+    x = eval(input("Enter a number between 0 and 1: "))
+    for i in range(10):
+        x = 3.9 * x * (1-X)
+        print(x)
+
+main()  
+
